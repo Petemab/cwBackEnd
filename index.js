3ec0a3be911296ea9b685abe8094cad3afdee89a
Petemab <peteben18@hotmail.com>
2018-09-19 14:19:42 +0100

    changed start and other couple of things

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-require('dotenv').config();
-const env = process.env.NODE_ENV || 'dev';
+
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 //---server data
 const port = process.env.PORT || 8000;
 const people = require('./routes/people');
+
+
+app.use(express.static('public'));
 
 //---json-parser config
 // app.use(bodyParser.urlencoded({ extended: false }));
@@ -15,5 +17,5 @@ app.use('/api', people);
 
 //---server listener
 app.listen(port, () => {
-  console.log('Server is up and running on localhost:8000');
+  console.log('Server is up and running !');
 });
